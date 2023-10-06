2d4e934cb397952f323f2d4241cfd7099df491b4
Sneha1690 <147036429+Sneha1690@users.noreply.github.com>
2023-10-06 12:04:39 +0530

    Update program_1.py

--- program_1.py
+++ program_1.py
@@ -1,2 +1,8 @@
-print('hello world')
+#summation
+def (x,y):
+    c = x+y
+    return c
+x = int (input ("Enter 1st number :"))
+y = int(input("Enter 2nd number :"))
+print("The number is -",def(x,y))
 
